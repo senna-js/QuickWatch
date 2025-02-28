2b6701c3ffae02493e21c6c572902a753afac2cd
varunaditya-plus <varunaditya.aga@gmail.com>
2025-02-28 13:29:56 +0100

    Removed comments, added favicon

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-// QuickWatch - Main Entry Point
 import './style.css'
 import './spinner.css'
 import { initRouter } from './router.js'
 
-// Initialize the application when DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
-  // Initialize the router
   initRouter();
 });
