ce8d1c0d4f4d7f12caa541aea9d3830e96f21248
= <=>
2025-04-27 13:00:37 +0200

    More proper tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {
-    allowedHosts: ['api.varunaditya.xyz']
-  }
+  server: {allowedHosts: ['api.varunaditya.xyz']},
+  plugins: [tailwindcss(),]
 });
